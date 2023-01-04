0bc9f8af9a56bef43dbdae98454b734668c61b0d
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-04 20:33:18 +0530

    Added: Calculating Length

--- com/bridgelabz/LineComparison/LineCompression.java
+++ com/bridgelabz/LineComparison/LineCompression.java
@@ -1,10 +1,25 @@
 package com.bridgelabz.LineComparison;
 
+import java.util.Scanner;
+
 public class LineCompression {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Line Compression Program");
-		
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of X1 X2 Y1 Y2");
+
+		int x1 = sc.nextInt();
+		int x2 = sc.nextInt();
+		int y1 = sc.nextInt();
+		int y2 = sc.nextInt();
+
+		double length;
+
+		length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		System.out.println("Length of line: " + length);
+
 	}
 
 }
