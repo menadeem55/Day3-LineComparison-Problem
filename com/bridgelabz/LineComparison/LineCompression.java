6c0023b47d05771bbec3fcfb3c4389ac5bb721bf
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-04 20:28:56 +0530

    Added: welcome message

---
+++ com/bridgelabz/LineComparison/LineCompression.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.LineComparison;
+
+public class LineCompression {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Line Compression Program");
+		
+	}
+
+}
